747b9adffc4cbc1f42e6f929ce39b10d33e9179b
Wilkinson Maciel <wilkinson.58.35@gmail.com>
2024-10-22 17:35:05 -0300

    types of response finished

--- ast_comp2.py
+++ ast_comp2.py
@@ -19,14 +19,22 @@ x = r'''def knapsack(capacity, items):
     return memo[len(items), capacity]'''
 
 y = r'''def knapsack(capacity, items):
-    memo = [0] * (capacity + 1)  # 1D array for memoization
+    from collections import defaultdict
+    memo = defaultdict(int)
 
-    for item in items:
-        weight, value = item
-        for j in range(capacity, weight - 1, -1):
-            memo[j] = max(memo[j], memo[j - weight] + value)
+    for i in range(1, len(items) + 1):
+        weight, value = items[i - 1]
 
-    return memo[capacity]'''
+        for j in range(1, capacity + 1):
+            memo[i, j] = memo[i - 1, j]
+
+            if weight <= j:
+                memo[i, j] = max(
+                    memo[i, j],
+                    value + memo[i - 1, j - weight]
+                )
+
+    return memo[len(items), capacity]'''
 
 xd = ast.dump(ast.parse(x))
 yd = ast.dump(ast.parse(y))
