d10f94337c6ef2fe44c182660fb02b430a8df10d
Wilkinson Maciel <wilkinson.58.35@gmail.com>
2024-10-25 17:43:50 -0300

    run.sh updated to write the output in a file

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-run() {
-  echo "Running $1..."
-}
-
 if [ $# -eq 0 ]; then
   echo "Error: No name provided"
   exit 1
 fi
 
-run "$1"
+echo "Run $1 in execution $2"
 
-pytest QuixBugs/python_testcases/test_$1.py
+output_file="/home/wilkinson/Documents/TCC/TG/Results/python_programs_run$2/$1/notes.txt"
 
-python3 ast_comp.py $1
+command1="pytest QuixBugs/python_testcases/test_$1.py"
+
+command2="python3 ast_comp.py $1"
+
+$command1 > $output_file 2>&1
+$command2 >> $output_file 2>&1
